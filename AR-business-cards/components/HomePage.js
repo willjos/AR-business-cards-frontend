3973604a207057d4d331aada94246235e6dbc3f1
Swastik Dilip <justsyd224@gmail.com>
2023-01-12 12:57:36 +0000

    Created conditional rendering for AR cards

--- AR-business-cards/components/HomePage.js
+++ AR-business-cards/components/HomePage.js
@@ -1,13 +1,33 @@
 import { StyleSheet, View, Text, Button, TextInput } from "react-native";
 import { useState } from "react";
+import ArCardView from "./ArCardView";
 
 export default function HomePage() {
+  const [viewCard, setViewCard] = useState(false);
+
+  function handleViewCardPress() {
+    setViewCard(true);
+  }
+
   return (
     <View>
       <Text>Hello, UserName!</Text>
-      <Button title="Make Card" />
-      <Button title="View your QR Code" />
-      <Button title="View Business Card" />
+      {/* <Button title="Make Card" />
+      <Button title="View your QR Code" /> */}
+      {viewCard ? (
+        <View style={styles.CameraContainer}>
+          <ArCardView />
+        </View>
+      ) : (
+        <Button title="View Business Card" onPress={handleViewCardPress} />
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  CameraContainer: {
+    height: "100%",
+    width: "100%",
+  },
+});
