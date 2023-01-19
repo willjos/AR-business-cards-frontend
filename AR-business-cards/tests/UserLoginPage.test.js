6d0d469891aab894325df681601720a5ac94ec7a
Swastik Dilip <justsyd224@gmail.com>
2023-01-19 12:14:40 +0000

    Configurations and silenced warning

--- AR-business-cards/tests/UserLoginPage.test.js
+++ AR-business-cards/tests/UserLoginPage.test.js
@@ -2,6 +2,8 @@ import * as React from "react";
 import { render, screen, fireEvent } from "@testing-library/react-native";
 import UserLoginPage from "../components/UserLoginPage";
 
+jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
+
 describe("User Login Screen", () => {
   test("Log In Button appears", () => {
     //When styling is pushed to main, change buttons to pressables
