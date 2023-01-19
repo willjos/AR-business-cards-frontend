36fde30aee06d99f364a5f1fc0347e484e855065
Swastik Dilip <justsyd224@gmail.com>
2023-01-19 10:11:19 +0000

    Did necessary configuration

--- AR-business-cards/babel.config.js
+++ AR-business-cards/babel.config.js
@@ -1,6 +1,13 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
   };
+};
+
+module.exports = {
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    ["@babel/preset-react", { targets: { node: "current" } }], // add this
+  ],
 };
