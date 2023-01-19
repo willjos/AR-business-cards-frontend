2036497b7578310cbf3d194c5be4009d80285ad7
Swastik Dilip <justsyd224@gmail.com>
2023-01-19 16:28:12 +0000

    Some config

--- AR-business-cards/babel.config.js
+++ AR-business-cards/babel.config.js
@@ -1,6 +1,14 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
   };
+};
+
+module.exports = {
+  presets: [
+    ["module:metro-react-native-babel-preset"],
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    ["@babel/preset-react", { targets: { node: "current" } }], // add this
+  ],
 };
